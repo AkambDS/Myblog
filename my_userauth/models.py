294e0248c93a36fbc23a42b89906175c07702d73
shareya <shareya@gmail.com>
2020-03-02 11:07:42 -0500

    initial done

--- my_userauth/models.py
+++ my_userauth/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+# chabges
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
